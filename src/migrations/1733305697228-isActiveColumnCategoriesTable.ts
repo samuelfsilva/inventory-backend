78b5cbdbb89c69e3ba741f4c222e7486de6ade05
Samuel Ferreira <samuel.ferreira.ti@gmail.com>
2024-12-30 17:59:27 -0300

    add isActive column to categories table

--- src/migrations/1733305697228-isActiveColumnCategoriesTable.ts
+++ src/migrations/1733305697228-isActiveColumnCategoriesTable.ts
@@ -1,20 +1,22 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
-export class IsActiveColumnCategoriesTable1733305697228 implements MigrationInterface {
-    name = 'IsActiveColumnCategoriesTable1733305697228'
+export class IsActiveColumnCategoriesTable1733305697228
+  implements MigrationInterface
+{
+  name = "IsActiveColumnCategoriesTable1733305697228";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "temporary_categories" ("id" varchar PRIMARY KEY NOT NULL, "description" varchar NOT NULL, "isActive" boolean NOT NULL)`);
-        await queryRunner.query(`INSERT INTO "temporary_categories"("id", "description") SELECT "id", "description" FROM "categories"`);
-        await queryRunner.query(`DROP TABLE "categories"`);
-        await queryRunner.query(`ALTER TABLE "temporary_categories" RENAME TO "categories"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "categories",
+      new TableColumn({
+        name: "isActive",
+        type: "boolean",
+        isNullable: false,
+      })
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "categories" RENAME TO "temporary_categories"`);
-        await queryRunner.query(`CREATE TABLE "categories" ("id" varchar PRIMARY KEY NOT NULL, "description" varchar NOT NULL)`);
-        await queryRunner.query(`INSERT INTO "categories"("id", "description") SELECT "id", "description" FROM "temporary_categories"`);
-        await queryRunner.query(`DROP TABLE "temporary_categories"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("categories", "isActive");
+  }
 }
